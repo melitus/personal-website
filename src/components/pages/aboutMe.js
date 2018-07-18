35c62fe0d55b0723b56224039b4022cb06b84096
Engr.Sunday <asmelitus@gmail.com>
2018-07-18 21:33:58 +0100

    added ccontent to about page

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class About extends Component{
  render() {
    return(
-     <div>
-        <h1>About Page</h1>
-     </div>
+     <div className="contact-body">
+       <Grid className="contact-grid">
+         <Cell col={6}>
+           <h2>Sunday Aroh</h2>
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Full Stack Web Developer using MongoDB, Express, React, Node.js (MERN) with an emphasis on Frontend web development
+              and mobile using React & React Native. Experienced in building CRUD applications, RESTful APIs using Node.js
+              and express, User Auth and oAuth with PassportJS and Firebase. Common libraries of use: Axios, Redux, Mongoose,
+              Lodash, Express, body-parser, socket.io, react-router, redux- thunk. Common use of UI libraries: Bootstrap 4,
+              React-Bootstrap, MaterializeCSS, Material-UI, Elemental-UI and Native-Base for React Native.
+              </p>
+         </Cell>
+         </Grid>
+        </div>
    )
  }
 }
--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
 
 
 class Resume extends Component {
@@ -33,8 +35,51 @@ class Resume extends Component {
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
           </Cell>
           <Cell className="resume-right-col" col={8}>
-            
-
+            <h2>Education</h2>
+              <Education
+                startYear={2002}
+                endYear={2006}
+                schoolName="My University"
+                schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                 />
+                 <Education
+                   startYear={2007}
+                   endYear={2009}
+                   schoolName="My 2nd University"
+                   schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                    />
+                <hr style={{borderTop: '3px solid #e22947'}} />
+              <h2>Experience</h2>
+                  <Experience
+                    startYear={2009}
+                    endYear={2012}
+                    jobName="First Job"
+                    jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                    />
+                    <Experience
+                      startYear={2012}
+                      endYear={2016}
+                      jobName="Second Job"
+                      jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                      />
+              <hr style={{borderTop: '3px solid #e22947'}} />
+              <h2>Skills</h2>
+                  <Skills
+                    skill="javascript"
+                    progress={100}
+                    />
+                    <Skills
+                      skill="HTML/CSS"
+                      progress={80}
+                      />
+                      <Skills
+                      skill="NodeJS"
+                      progress={50}
+                      />
+                      <Skills
+                        skill="React"
+                        progress={25}
+                      />
           </Cell>
         </Grid>
       </div>
