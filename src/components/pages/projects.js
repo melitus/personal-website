232ab2b0a1b6f68c0f13af071b930acdae793cc9
Engr.Sunday <asmelitus@gmail.com>
2018-07-18 14:54:14 +0100

    added tabs to project links

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,13 +1,48 @@
 import React, {Component} from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component{
- render() {
-   return(
-     <div>
-        <h1>Projects Page</h1>
-     </div>
-   )
- }
+  constructor(props){
+    super(props)
+
+    this.state ={
+      activeTab:0
+    };
+  }
+  onchangeTabListerner = tabId => {
+    this.setState({
+      activeTab: tabId
+    })
+  }
+toggleCategories(){
+  if(this.state.activeTab == 0){
+    return(
+      <div>
+        <h1>This is react</h1>
+      </div>
+    )
+  }
+
 }
+
+  render(){
+    return(
+      <div className="category-tab">
+        <Tabs activeTab={this.state.activeTab} onChange={this.onchangeTabListerner}>
+           <Tab>React</Tab>
+           <Tab>React-Native</Tab>
+           <Tab>MongoDb</Tab>
+           <Tab>Rest Api</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+        {this.toggleCategories()}
+        </section>
+      </div>
+    )
+  }
+
+ }
+
 
 export default Projects;
