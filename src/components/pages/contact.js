806e4bd7b7fd68530a12e0ac6e87d1039fe682ca
Engr.Sunday <asmelitus@gmail.com>
2018-07-18 20:57:15 +0100

    finished contact page designs

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,17 +1,55 @@
 import React, {Component} from 'react';
-import { Grid, Cell} from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
-class Contact extends Component{
- render() {
-   return(
-     <div className="contact-body">
-        <Grid>
-           <Cell col={6}>half page</Cell>
-           <Cell col={6}>half page</Cell>
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Sunday Aroh</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    (234) 8061329356
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-fax" aria-hidden="true"/>
+                    (234) 8061329356
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    asmelitus@gmail.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-skype" aria-hidden="true"/>
+                    melisunday
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
         </Grid>
-     </div>
-   )
- }
+      </div>
+    )
+  }
 }
-
 export default Contact;
