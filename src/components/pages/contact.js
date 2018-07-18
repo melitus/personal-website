4474a3c2612512220a0ddb35dec2b15fff647234
Engr.Sunday <asmelitus@gmail.com>
2018-07-18 20:44:48 +0100

    addded fonts google link to public folder

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import { Grid, Cell} from 'react-mdl';
 
 class Contact extends Component{
  render() {
    return(
-     <div>
-        <h1>Contact Page</h1>
+     <div className="contact-body">
+        <Grid>
+           <Cell col={6}>half page</Cell>
+           <Cell col={6}>half page</Cell>
+        </Grid>
      </div>
    )
  }
